3cd1d5b773118106ea6a1b7b0fc34d24b2dc0254
cloudycrane <cloudycrane@hotmail.com>
2025-06-17 15:37:37 +0800

    configparser.SafeConfigParser

--- versioneer.py
+++ versioneer.py
@@ -396,12 +396,13 @@ def get_config_from_root(root):
     # configparser.NoOptionError (if it lacks "VCS="). See the docstring at
     # the top of versioneer.py for instructions on writing your setup.cfg .
     setup_cfg = os.path.join(root, "setup.cfg")
-    try:
-        parser = configparser.SafeConfigParser()
-    except Exception as e:
-        parser = configparser.ConfigParser()
     with open(setup_cfg, "r") as f:
-        parser.readfp(f)
+        try:
+            parser = configparser.SafeConfigParser()
+            parser.readfp(f)
+        except Exception as e:
+            parser = configparser.ConfigParser()
+            parser.read_file(f)
     VCS = parser.get("versioneer", "VCS")  # mandatory
 
     def get(parser, name):
